ede443f9b2d24268d80182b395ac1932862df21f
aashishkumar99info <aashishkumar99info@gmail.com>
2023-04-24 16:15:36 +0530

    Adding Expenses for tracking purpose

--- expense-tracker/src/components/Pages/HomePage/Home.js
+++ expense-tracker/src/components/Pages/HomePage/Home.js
@@ -1,10 +1,13 @@
 import './Home.css'
 import { useNavigate, useParams } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../../Contexts/AppContext';
+import ExpenseForm from './ExpenseForm/ExpenseForm';
+import ExpenseList from './Expenses/ExpensesList';
 
 function Home() {
     const ctx = useContext(AppContext)
+    const [expenseList, setExpenseList] = useState([])
     const params = useParams();
     const navTo = useNavigate();
     console.log(ctx.idToken)
@@ -18,15 +21,23 @@ function Home() {
         ctx.setidToken(null);
         navTo('/');
     }
+    const formSubmithandler = (obj) => {
+
+        setExpenseList((prevList) => [obj, ...prevList])
+    }
 
     return (
-        <div>
-            <div className="welcome">
-                <p>Welcome To Your Expense Tracker !!!</p>
-                <button className='button-logout' onClick={logoutHandler} >Logout</button>
-                <button className='login-card' onClick={() => navTo(`/profile/${ctx.idToken}`)}>  Your Profile Is Incomplete ! Complete Now </button>
+        <>
+            <div>
+                <div className="welcome">
+                    <p>Welcome To Your Expense Tracker !!!</p>
+                    <button className='button-logout' onClick={logoutHandler} >Logout</button>
+                    <button className='login-card' onClick={() => navTo(`/profile/${ctx.idToken}`)}>  Your Profile Is Incomplete ! Complete Now </button>
+                </div>
             </div>
-        </div>
+            <ExpenseForm onSubmit={formSubmithandler}></ExpenseForm>
+            <ExpenseList data={expenseList}></ExpenseList>
+        </>
     )
 }
 
