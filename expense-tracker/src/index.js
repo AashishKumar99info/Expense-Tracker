a908b20653696d222778e0421ee7a3beb459a43b
aashishkumar99info <aashishkumar99info@gmail.com>
2023-04-28 16:54:19 +0530

    Using redux toolkit Doing the Expense Tracker the right way

--- expense-tracker/src/index.js
+++ expense-tracker/src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import ContextProvider from './components/Contexts/AppContext';
+import { Provider } from 'react-redux';
+import store from './components/States/store';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <ContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ContextProvider>
+    </Provider>
   </React.StrictMode>
 );
 
